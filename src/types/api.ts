4c56ff4e3e0dce15a0540af56dcbd139c627e9fb
Praveen <pvnsripati@gmail.com>
2025-07-23 13:41:23 +0530

    Remove unwanted files

--- src/types/api.ts
+++
@@ -1,9 +0,0 @@
-// Define the shape of the data we expect to return from the parse function
-export interface ParsedArticle {
-  title: string;
-  content: string; // The article's HTML content
-  textContent: string; // The article's text content
-  excerpt: string; // A short summary
-  byline: string; // The author or site name
-  domain: string;
-}
